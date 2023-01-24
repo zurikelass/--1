0058ce1aa5009e65d2e95c534044211fa11545a6
zurikelass <zuragogberashvili93@gmail.com>
2023-01-24 15:10:08 +0400

    წინ ვერ წავედი

--- lovevue/src/App.vue
+++ lovevue/src/App.vue
@@ -1,47 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import List from './List.vue'
+  export default{
+    components:{
+      List
+    },
+    data(){
+      return{
+        pageTitle:"NavBar",
+        myNotes:[
+         {
+          id:1,
+          title: "Menu"
+         },
+         {
+          id:2,
+          tittle: "Contact"
+         } ,
+        {
+          id:3,
+          tittle: "About Us"
+        },
+        ]
+      }
+    
+    } 
+    
+  }
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! right" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{pageTitle}}</h1>
+  <List :list_data="myNotes"/>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ lovevue/src/List.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    props:{
+        data:{ type:Array, required:true}
+    }
+}
+
+</script>
+<template>
+    <ul>
+        <li v-for="item in data" :key="item.id">{{ item.title }}</li>
+    </ul>
+</template>
